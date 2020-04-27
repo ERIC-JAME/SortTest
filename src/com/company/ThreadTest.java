ee0c3099c13bc221a37a9cbbe3b8917711600d31
ERIC-JAME <55659811+ERIC-JAME@users.noreply.github.com>
2020-04-27 20:46:41 +0800

    Update ThreadTest.java

--- src/com/company/ThreadTest.java
+++ src/com/company/ThreadTest.java
@@ -7,11 +7,14 @@ public class ThreadTest {
     public static Integer getNum(){
         return num ++;
     }
-
+public static char getLetter(){
+        return (char)((num++)+96);
+    }
     public static void main(String[] args) {
         for (int i = 1; i <=100 ; i++) {
             new Thread(() -> {
                 System.out.println(Thread.currentThread().getName() + "=====" + ThreadTest.getNum());
+                System.out.println(Thread.currentThread().getName() + "=====" + ThreadTest.getLetter());
             }).start();
         }
     }
